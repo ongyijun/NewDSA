31ed0e70dcd3757be06f3633a9563c8384ceb953
HP <HP@LAPTOP-F9K51DAD>
2017-12-24 17:56:29 +0800

    Module A(Not Completed)

---
+++ src/ADT/AListInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ADT;
+
+/**
+ *
+ * @author HP
+ */
+public interface AListInterface<T> {
+    public boolean isEmpty();
+    public int getNumOfEntries();
+    public void clear();
+    public void add(T entry);
+    public T get(int pos);
+    public boolean replace(int pos, T entry);
+    public void delete(int pos);
+    public void sortNewest();
+    public void sortPromotional();
+}
