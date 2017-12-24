05a38ab234e0e15862179fc508790284ee67fe96
Pee <Pee@DragonzPee>
2017-12-25 06:24:59 +0800

    Module D

--- src/ADT/ModuleDInterface.java
+++ src/ADT/ModuleDInterface.java
@@ -4,7 +4,6 @@ import java.util.Calendar;
 
 public interface ModuleDInterface<T>
 {
-    //Basic Operation==================
     public void add(T newEntry);
     public T get(int position);
     public int getTotalEntries();
@@ -12,13 +11,10 @@ public interface ModuleDInterface<T>
     public boolean isEmpty();
     public void clear();
     public boolean update(int index, T Entry);
-    //Basic Operation==================
-    //My Operation=====================
     public boolean updateDMClockInOut(String DMID);
     public void display();
     public void displayCustomerPendingOrderInTable(String CustID);
     public void displayCustomerAssignOrderInTable(String CustID);
     public void displayCustomerDeliverOrderInTable(String CustID);
     public void SortTotalDelivery();
-    //My Operation=====================
 }
